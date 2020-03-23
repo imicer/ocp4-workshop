06c8a6deebe967a75bcac349d48a9c7cc7cbaeab
Samuel Veloso <veloso.lopez.samuel@gmail.com>
2020-03-23 14:09:55 +0100

    Fix error: x509 certificate signed by unknown authority

--- 06-api-server/install.sh
+++ 06-api-server/install.sh
@@ -5,9 +5,9 @@ set -o nounset  # exit when use undeclared variables
 set -o pipefail # return the exit code of the last command that threw a non-zero
 
 # Generate a new certificate for the API server using letsencrypt
-OCP_API_SERVER="$(oc whoami --show-server |\
+export OCP_API_SERVER="$(oc whoami --show-server |\
     sed -e "s/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/")"
-OCP_API_CERTS="$(pwd)/certificates"
+export OCP_API_CERTS="$(pwd)/certificates"
 
 if [ ! -f "${OCP_API_CERTS}/tls.key" ]; then
   letsencrypt certonly --manual --preferred-challenges dns \
@@ -22,6 +22,11 @@ fi
 oc create secret tls api-server-public-cert \
   --cert="${OCP_API_CERTS}/tls.crt" --key="${OCP_API_CERTS}/tls.key" \
   --namespace openshift-config || true
+
+# Get OAuth certificate (https://access.redhat.com/solutions/4505101)
+OAUTH_POD="$(oc get pods -l app=oauth-openshift -o jsonpath="{.items[0].metadata.name}" -n openshift-authentication)"
+oc rsh -n openshift-authentication ${OAUTH_POD} \
+    cat /run/secrets/kubernetes.io/serviceaccount/ca.crt > ~/.kube/${OCP_API_SERVER}.crt
 
 # Replace certificate
 envsubst < api-server/cluster.yml.tpl | oc apply -f -
